c728bcba9cc86bc20e6d73525cbd39bde97d10af
Adam Ploof <adamploof@hotmail.com>
2025-04-01 23:06:28 -0400

    implement alert signup form and handle submission, persist new alert criteria for users

--- GunksAlert.Api/Controllers/AlertController.cs
+++ GunksAlert.Api/Controllers/AlertController.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 using GunksAlert.Api.Data;
 using GunksAlert.Api.Models;
 using GunksAlert.Api.Services;
 using GunksAlert.Api.ViewModels;
+using GunksAlert.Api.Security;
 
 namespace GunksAlert.Api.Controllers;
 
@@ -13,29 +17,61 @@ public class AlertController : Controller {
     private readonly GunksDbContext _context;
     private readonly ConditionsChecker _conditionsChecker;
     private readonly AlertManager _alertManager;
+    private readonly UserManager<AppUser> _userManager;
+
     private readonly ILogger<AlertController> _logger;
 
     public AlertController(
         GunksDbContext context,
         ConditionsChecker conditionsChecker,
         AlertManager alertManager,
+        UserManager<AppUser> userManager,
         ILogger<AlertController> logger
     ) {
         _context = context;
         _conditionsChecker = conditionsChecker;
         _alertManager = alertManager;
+        _userManager = userManager;
         _logger = logger;
     }
 
-    [Route("alert/sign-up", Name="AlertSignup")]
-    public IActionResult Signup(AlertSignupViewModel model) {
+    [HttpGet("alert/sign-up", Name="AlertSignup")]
+    public IActionResult Signup() {
+        return View();
+    }
+
+    [HttpPost("alert/sign-up", Name="AlertSignupSubmit")]
+    public async Task<IActionResult> Signup(AlertSignupViewModel model) {
         if (!ModelState.IsValid) {
             return View(model);
         }
 
-        // TODO: convert response to AlertCriteria
+        AppUser? user = await _userManager.GetUserAsync(User);
+        if (user == null) {
+            // This shouldn't ever happen
+            return Unauthorized();
+        }
 
-        return View(model);
+        AlertPeriod period = new AlertPeriod();
+        // Not allowing the user to set start/end dates for now
+        period.StartDate = DateTimeOffset.MinValue;
+        period.EndDate = DateTimeOffset.MaxValue;
+        period.SetMonths(model.GetMonthNames());
+        period.SetDaysOfWeek(model.GetWeekdayNames());
+        _context.AlertPeriods.Add(period);
+
+        Crag crag = _context.Crags.Where(c => c.Name == "Gunks").First();
+        ClimbableConditions conditions = _context.ClimbableConditions.First();
+        AlertCriteria criteria = new AlertCriteria() {
+            Crag = crag,
+            AlertPeriod = period,
+            ClimbableConditions = conditions
+        };
+        user.AddCriteria(criteria);
+
+        _context.SaveChanges();
+
+        return RedirectToAction("Index", "Home");
     }
 
     [Route("alert/process-alerts/{cragId}")]
--- GunksAlert.Api/ViewModels/AlertSignupViewModel.cs
+++ GunksAlert.Api/ViewModels/AlertSignupViewModel.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
 using GunksAlert.Api.Models;
 
 namespace GunksAlert.Api.ViewModels;
@@ -30,4 +32,36 @@ public class AlertSignupViewModel {
     public bool October { get; set; } = false;
     public bool November { get; set; } = false;
     public bool December { get; set; } = false;
+
+    // Not elegant. But simple ¯\_(ツ)_/¯
+    public List<string> GetWeekdayNames() {
+        List<string> days = [];
+        if (Sunday) days.Add("Sunday");
+        if (Monday) days.Add("Monday");
+        if (Tuesday) days.Add("Tuesday");
+        if (Wednesday) days.Add("Wednesday");
+        if (Thursday) days.Add("Thursday");
+        if (Friday) days.Add("Friday");
+        if (Saturday) days.Add("Saturday");
+
+        return days;
+    }
+
+    public List<string> GetMonthNames() {
+        List<string> months = [];
+        if (January) months.Add("January");
+        if (February) months.Add("February");
+        if (March) months.Add("March");
+        if (May) months.Add("May");
+        if (May) months.Add("May");
+        if (June) months.Add("June");
+        if (July) months.Add("July");
+        if (August) months.Add("August");
+        if (September) months.Add("September");
+        if (October) months.Add("October");
+        if (November) months.Add("November");
+        if (December) months.Add("December");
+
+        return months;
+    }
 }
